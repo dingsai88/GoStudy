79fc2f8b51b29873643b3bfaa63534f61e14e64d
saiding@creditease.cn <saiding@creditease.cn>
2022-06-01 18:00:20 +0800

    feat:go语言基础语法

---
+++ GoSwitch.go
@@ -0,0 +1,117 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	switch {
+	case false:
+		fmt.Println("1、case 条件语句为 false")
+		fallthrough
+	case true:
+		fmt.Println("2、case 条件语句为 true")
+		fallthrough
+	case false:
+		fmt.Println("3、case 条件语句为 false")
+		fallthrough
+	case true:
+		fmt.Println("4、case 条件语句为 true")
+	case false:
+		fmt.Println("5、case 条件语句为 false")
+		fallthrough
+	default:
+		fmt.Println("6、默认 case")
+	}
+
+	/* 定义局部变量 */
+	var grade string = "B"
+	var marks int = 90
+
+	switch marks {
+	case 90:
+		grade = "A"
+	case 80:
+		grade = "B"
+	case 50, 60, 70:
+		grade = "C"
+	default:
+		grade = "D"
+	}
+
+	switch {
+	case grade == "A":
+		fmt.Printf("优秀!\n")
+	case grade == "B", grade == "A":
+		fmt.Printf("良好\n")
+	case grade == "D":
+		fmt.Printf("及格\n")
+	case grade == "F":
+		fmt.Printf("不及格\n")
+	default:
+		fmt.Printf("差\n")
+	}
+	fmt.Printf("你的等级是 %s\n", grade)
+
+	var c1, c2, c3 chan int
+	var i1, i2 int
+	select {
+	case i1 = <-c1:
+		fmt.Printf("received ", i1, " from c1\n")
+	case c2 <- i2:
+		fmt.Printf("sent ", i2, " to c2\n")
+	case i3, ok := (<-c3): // same as: i3, ok := <-c3
+		if ok {
+			fmt.Printf("received ", i3, " from c3\n")
+		} else {
+			fmt.Printf("c3 is closed\n")
+		}
+	default:
+		fmt.Printf("no communication\n")
+	}
+
+	// 不使用标记
+	fmt.Println("---- break ----")
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("i: %d\n", i)
+		for i2 := 11; i2 <= 13; i2++ {
+			fmt.Printf("i2: %d\n", i2)
+			break
+		}
+	}
+
+	// 使用标记
+	fmt.Println("---- break label ----")
+re:
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("i: %d\n", i)
+		for i2 := 11; i2 <= 13; i2++ {
+			fmt.Printf("i2: %d\n", i2)
+			break re
+		}
+	}
+
+	fmt.Println("---- break label 2----")
+
+	// 不使用标记
+	fmt.Println("---- continue ---- ")
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("i: %d\n", i)
+		for i2 := 11; i2 <= 13; i2++ {
+			fmt.Printf("i2: %d\n", i2)
+			continue
+		}
+	}
+
+	// 使用标记
+	fmt.Println("---- continue label ----")
+re2:
+	for i := 1; i <= 3; i++ {
+		fmt.Printf("i: %d\n", i)
+		for i2 := 11; i2 <= 13; i2++ {
+			fmt.Printf("i2: %d\n", i2)
+			continue re2
+		}
+	}
+	fmt.Println("---- continue label end ----")
+
+}
--- HelloWorld.go
+++ HelloWorld.go
@@ -2,6 +2,64 @@ package main
 
 import "fmt"
 
+/**
+I.运行源文件:
+go run HelloWorld.go
+
+I.编译成二进制exe：
+ go build HelloWorld.go
+
+II.执行编译完的程序:HelloWorld
+
+*/
 func main() {
+	//普通打印
 	fmt.Println("Hello, World!")
+
+	//占位符测试
+	var stockcode = 1238
+	var enddate = "2020-12-3144484"
+	var url = "Code=%d&endDate=%s"
+	var target_url = fmt.Sprintf(url, stockcode, enddate)
+	fmt.Println("占位符测试" + target_url)
+
+	//定义一个变量
+	var str string = "stringstr"
+	fmt.Println("定义单个变量1:" + str)
+	fmt.Println("定义单个变量2:", str)
+
+	//定义多个变量
+	var a, b int = 1, 2
+	fmt.Println("定义多个变量:", a+b)
+
+	//变量未初始化
+	var strNoInit string
+	fmt.Println("变量未初始化string:" + strNoInit)
+
+	var intNoInit int
+	fmt.Println("变量未初始化int:", intNoInit)
+
+	var boNoInit bool
+	fmt.Println("变量未初始化bool:", boNoInit)
+
+	var intVal int
+	intVal2 := 1
+	fmt.Println("变量未初始化intVal2:", intVal, intVal2)
+
+	//变量未初始化
+	var strEquals1 string = "abc"
+	var strEquals2 string
+	var strEquals3 *string
+	//内存中将 i 的值进行了拷贝：
+	strEquals2 = strEquals1
+	fmt.Println("赋值判断1:", strEquals1, strEquals2)
+	strEquals1 = "xyz"
+	fmt.Println("赋值判断2:", strEquals1, strEquals2)
+
+	strEquals3 = &strEquals1
+
+	fmt.Println("赋值判断3 引用类型:", strEquals1, strEquals3)
+	strEquals1 = "xyz2"
+	fmt.Println("赋值判断3 引用类型2:", strEquals1, strEquals3)
+
 }
